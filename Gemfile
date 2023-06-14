6b1f7eb200ccf1ac51ae564a3b6bf0953f515879
Soumik Maity <soumik.maity@geogo.in>
2023-06-15 01:01:22 +0530

    changed html to haml

--- Gemfile
+++ Gemfile
@@ -74,3 +74,5 @@ end
 
 
 gem "devise" 
+gem 'haml-rails'
+gem 'tailwindcss-rails'
--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module Rails7Learn
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.generators.template_engine = :haml
 
     # Configuration for the application, engines, and railties goes here.
     #
