13187933d5db4b4288901c386b7f87b71de89fea
Soumik Maity <soumik.maity@geogo.in>
2023-06-24 14:23:47 +0530

    updated: turbo stream - create-update-delete

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -3,4 +3,15 @@ class Quote < ApplicationRecord
   validates_uniqueness_of :name
 
   scope :ordered, -> { order(id: :desc) }
+
+
+  after_create_commit -> { broadcast_prepend_later_to "quotes", partial: "quotes/quote", locals: { quote: self } }
+  after_update_commit -> { broadcast_replace_later_to "quotes", partial: "quotes/quote", locals: { quote: self } }
+
+  # after_create_commit -> { broadcast_prepend_later_to "quotes" }
+  # after_update_commit -> { broadcast_replace_later_to "quotes" }
+  after_destroy_commit -> { broadcast_remove_to "quotes" }
+
+
+
 end
