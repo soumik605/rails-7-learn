7b682f57ee4012464b352408084f210f30bb5c8a
Soumik Maity <soumik.maity@geogo.in>
2023-06-24 14:42:45 +0530

    updated

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -7,6 +7,7 @@ class Quote < ApplicationRecord
 
   after_create_commit -> { broadcast_prepend_later_to "quotes", partial: "quotes/quote", locals: { quote: self } }
   after_update_commit -> { broadcast_replace_later_to "quotes", partial: "quotes/quote", locals: { quote: self } }
+  after_update_commit -> { broadcast_replace_later_to self, partial: "quotes/quote_details", locals: { quote: self }, target: "quote_details_#{self.id}" }
 
   # after_create_commit -> { broadcast_prepend_later_to "quotes" }
   # after_update_commit -> { broadcast_replace_later_to "quotes" }
